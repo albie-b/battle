d7a70e88cbd044d59e2f7c487221d05850ea46d0
albie-b <55197619+albie-b@users.noreply.github.com>
2019-10-14 15:17:41 +0100

    just finished using forms

--- app.rb
+++ app.rb
@@ -1,4 +1,5 @@
 require 'sinatra'
+
 get '/' do
   'Hello'
 end
@@ -6,10 +7,18 @@ end
 get '/secret' do
   'no-where'
 end
+
 get '/hidden' do
   'nowhere'
 end
 
-get '/cat' do
-  erb(:index)
+get '/random-cat' do
+  @name = ["Amigo", "Oscar", "Viking"].sample
+  erb :index
+end
+
+get '/named-cat' do
+  p params
+  @name = params[:name]
+  erb :index
 end
